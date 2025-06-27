53644a44dbea684f870691df47d45dd02e09200a
mdsaharshital <mdsaharshitol@gmail.com>
2025-06-28 03:21:08 +0600

    first commit

---
+++ js/inject.js
@@ -0,0 +1,12 @@
+// js/inject.js
+document.addEventListener("DOMContentLoaded", () => {
+  const includes = document.querySelectorAll("[data-include]");
+  includes.forEach((el) => {
+    const file = el.getAttribute("data-include");
+    fetch(file)
+      .then((res) => res.text())
+      .then((data) => {
+        el.innerHTML = data;
+      });
+  });
+});
